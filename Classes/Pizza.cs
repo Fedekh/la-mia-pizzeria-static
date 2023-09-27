7f138ae744d38875ff89e207a88b45e411a80982
Federico Ceteroni <developer@overside.it>
2023-09-27 14:17:33 +0200

    Aggiungere i file di progetto.

---
+++ Classes/Pizza.cs
@@ -0,0 +1,28 @@
+﻿namespace la_mia_pizzeria_static.Classes
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo {  get; set; }
+        public double Price { get; set; }
+
+        public Pizza(string name, string description, string photo, double price)
+        {
+            Name = name;
+            Description = description;
+            Photo = photo;
+            Price = price;
+        }
+
+        public string GetPrice(double price)
+        {
+            return this.Price.ToString("0.00");
+        }
+
+        public string GetDescription(string description)
+        {
+            return this.Description;
+        }
+    }
+}
